70a8bcc93e735571d9a8ef4d7acc82eeac21ac91
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-25 08:16:14 +0900

    Update index.js gtag

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,22 @@ export default function Home({ allPostsData }){
           ))}
         </ul>
       </section>
+      
+    <!-- Google tag (gtag.js) -->
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=AW-11191525774"
+    ></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "AW-11191525774");
+    </script>
+      
     </Layout>
   )
 }
